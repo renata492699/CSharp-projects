780862c7d1a9265c23a31b2096ebcd32908ff7f5
Renata Urbanová <xurbano2@fi.muni.cz>
2023-05-05 19:10:05 +0200

    hw03

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 These are my school projects. 
 HW01: The task was to implement Tower, Projectile and Enemy classes (hw01-Assets-Scripts). 
 HW02: The task was to partly implement the console application. The application allows adding and removing products and categories and saving logs and analytic data.
+HW03: The task was to write LINQ queries to solve the tasks.
---
+++ hw03/Assignment/src/PV178.Homeworks.HW03/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Channels;
+using PV178.Homeworks.HW03;
+
+Console.WriteLine("Welcome to HW03, you can use this file as a playground for manually testing your solution.");
+
+Queries queries = new Queries();
+
+// Console.WriteLine(queries.SampleQuery());
+
+// var sharks = queries.TigerSharkAttackZipQuery();
+//
+// foreach (var s in sharks)
+// {
+//     Console.WriteLine(s);
+//
+// }
+
+Console.WriteLine(queries.NonFatalAttemptOfZambeziSharkOnPeopleBetweenDAndKQuery());
